dbc419d9ef50e3deb1f6d2bac4f72d987b40b489
Claudio <claudio@Claudios-MacBook-Pro.local>
2019-03-21 18:24:38 -0700

    Removed Swiper

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 const textToggleNext = document.querySelector(".swiper-button-next");
 const textTogglePrev = document.querySelector(".swiper-button-prev");
 
-const mySwiper = new Swiper(".swiper-container", {
-  direction: "horizontal",
-  loop: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
-  }
-});
 
 textToggleNext.addEventListener("click", () => {
     console.log('It works!');
