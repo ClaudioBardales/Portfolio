d1bf31c90f297e1f1e2d56f7cefa7c17bd5c84ce
Claudio <claudio@Claudios-MacBook-Pro.local>
2019-03-23 22:43:30 -0700

    Fixing portfolio for smaller laptops WIP

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const textToggleNext = document.querySelector(".swiper-button-next");
-const textTogglePrev = document.querySelector(".swiper-button-prev");
+// const textToggleNext = document.querySelector(".swiper-button-next");
+// const textTogglePrev = document.querySelector(".swiper-button-prev");
 
 
-textToggleNext.addEventListener("click", () => {
-    console.log('It works!');
-});
-textTogglePrev.addEventListener("click", () => {
-    console.log('it works!')
-});
+// textToggleNext.addEventListener("click", () => {
+//     console.log('It works!');
+// });
+// textTogglePrev.addEventListener("click", () => {
+//     console.log('it works!')
+// });
